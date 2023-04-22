d92b322b89ab040c9958fef5c687384ef8e0c9d3
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-04-21 19:56:19 -0500

    Primeros pasos de mi portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+// /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  theme: {
+    fontFamily: {
+      'sans': ['Gabriela'],
+      'serif': ['Elsie'],
+      'mono': ['Raleway'],
+      'montserrat': ['Montserrat'],
+      'poppins': ['Poppins'], 
+      'playfair': ['Playfair Display'],
+      
+    },
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      'green-300': '#84d9bc',
+      'orange-400': '#fb923c',
+      'orange-600': '#ea580c',
+      'oscuro': '#262626',
+      'indigo-200': '#c7d2fe',
+      'indigo-500': '#6366f1',
+      'pink-600': '#db2777',
+      'green-700': '#15803d',
+      'principal': '#fff5eb',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
