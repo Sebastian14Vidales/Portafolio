e5657618189e656a5290481559631b246f5a9a77
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-04-23 20:55:00 -0500

    Agregando animacion y ajustando el About me

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import './css/main.css'
 import './css/fuentes.css'
+import './js/scrollreveal'
+
+// Animación de mis componentes
+window.sr = ScrollReveal();
+sr.reveal('.texto-inicial', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px',
+    desktop: true,
+    mobile: true,
+    reset: true,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
 
