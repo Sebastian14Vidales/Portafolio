d9a03597c00f8242d95c156d636662d2ac3258ce
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-04-26 22:18:42 -0500

    Modificando Animaciones

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './js/scrollreveal';
 import './js/swiper-bundle.min.js'
 
 // Animación de mis componentes
-window.sr = ScrollReveal();
+const sr = ScrollReveal();
 sr.reveal('.texto-inicial', { duration: 2000, origin: 'left', distance: '-100px', desktop: true, mobile: true, reset: true, easing: 'cubic-bezier(0.5, 0, 0, 1)',});
 sr.reveal('.header, .foto-perfil', { duration: 2000, origin: 'bottom', distance: '-100px', desktop: true, mobile: true, reset: false, easing: 'cubic-bezier(0.5, 0, 0, 1)',});
 sr.reveal('.animacion-educacion-left', {duration: 3000, origin: 'right', distance: '-300px', desktop: true, mobile: true, reset: false, easing: 'cubic-bezier(0.5, 0, 0, 1)',})
