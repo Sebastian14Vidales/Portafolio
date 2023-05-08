870b127eed2670635b477ad9ed79864e560db93d
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-05-07 22:56:01 -0500

    Descargue PDF Finalizado

--- src/js/downloadPdf.js
+++ src/js/downloadPdf.js
@@ -1,4 +1,7 @@
 import pdf from '../assets/SebastianVidalesCV.pdf';
 
 const enlaceDownload = document.querySelector('.download');
+const enlaceDownload2 = document.querySelector('.download2');
+
 enlaceDownload.href = pdf;
+enlaceDownload2.href = pdf;
--- src/main.js
+++ src/main.js
@@ -14,3 +14,11 @@ const fechaSpan = document.querySelector('.fecha');
 let fecha = new Date();
 const year = fecha.getFullYear();
 fechaSpan.textContent = year;
+
+// Google Analytics
+window.dataLayer = window.dataLayer || []; 
+function gtag(){
+    dataLayer.push(arguments);
+} 
+gtag('js', new Date()); 
+gtag('config', 'G-PLYSS30L1D');
